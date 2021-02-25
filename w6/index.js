ed805c651c5a968b3bdc8ef8830392e1a2092cb3
Alsasa <zalsasa@labs.neit.edu>
2021-02-25 13:04:15 -0500

    w6 pay calculator

---
+++ w6/index.js
@@ -0,0 +1,10 @@
+<div id="container">
+<script>
+var hourly_pay = $14.5;
+var hours_work = 64;
+var tax_rate = .2;
+var gross_pay = hourly_pay * hours_work;
+var tax_cut = hourly_pay * hours_work * tax_rate;
+var net_pay = gross_pay - tax_cut;
+</script>
+</div>
